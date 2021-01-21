00152179300754d4f13cec206a20a67bf966ec36
OutboardTech76 <fme11@protonmail.com>
2021-01-21 03:59:10 +0100

    Added class Point

--- openposeMovement.py
+++ openposeMovement.py
@@ -4,6 +4,7 @@ import sys
 import cv2
 import os
 import argparse
+import numpy as np
 # import rospy
 from openpose import pyopenpose as op
 # import tensorflow as tf
@@ -14,6 +15,21 @@ from openpose import pyopenpose as op
 parser = argparse.ArgumentParser(description='Predict images using camera')
 parser.add_argument("--json", dest="jsonPath", type=str, default="jsonFiles/", help="Directory where save .json files (default = jsonFiles/)")
   
+# Point class with x,y,z values and point as a tuple (x,y)
+class Point():
+    def __init__(self,pt):
+        # Convert to numpy float or there will be error when printing lines
+        pt =  np.float32(pt)
+        self.x = pt[0]
+        self.y = pt[1]
+        self.score = pt[2]
+        self.point = (pt[0], pt[1])
+        if self.point > (0,0):
+            self.draw = True
+        else:
+            self.draw = False
+   
+    
 def setParams():
     args = parser.parse_args()
     params = dict()
@@ -23,8 +39,19 @@ def setParams():
     params["face"] = False
     # Reduce net resolution in order to avoid memory problems
     params["net_resolution"] = "-1x128"
-    params["write_json"] = args.jsonPath 
+    # params["write_json"] = args.jsonPath 
     return params
+
+def setReferenceFrame(data):
+    persons = len(data.poseKeypoints)
+    for i in range(persons):
+    try:
+        print("First dimension : ")
+        print(data.poseKeypoints[1][1])
+        print("done")
+    except:
+        pass
+
 
 
 if __name__ == '__main__':
@@ -51,6 +78,7 @@ if __name__ == '__main__':
         datum = op.Datum()
         datum.cvInputData = frame
         opWrapper.emplaceAndPop(op.VectorDatum([datum]))
+        setReferenceFrame(datum)
 
         output = datum.cvOutputData
         cv2.imshow('output', output)
