338c32912a069088114a50abc9f84ad6d598c2d3
OutboardTech76 <fme11@protonmail.com>
2021-01-21 05:06:32 +0100

    Added Pose class and function to change ref frame

--- openposeMovement.py
+++ openposeMovement.py
@@ -11,6 +11,34 @@ from openpose import pyopenpose as op
 # from tensorflow.compat.v1 import ConfigProto
 # from tensorflow.compat.v1 import InteractiveSession
 
+# For model BODY_25 :
+#   {0,  "Nose"},
+#   {1,  "Neck"},
+#   {2,  "RShoulder"},
+#   {3,  "RElbow"},
+#   {4,  "RWrist"},
+#   {5,  "LShoulder"},
+#   {6,  "LElbow"},
+#   {7,  "LWrist"},
+#   {8,  "MidHip"},
+#   {9,  "RHip"},
+#   {10, "RKnee"},
+#   {11, "RAnkle"},
+#   {12, "LHip"},
+#   {13, "LKnee"},
+#   {14, "LAnkle"},
+#   {15, "REye"},
+#   {16, "LEye"},
+#   {17, "REar"},
+#   {18, "LEar"},
+#   {19, "LBigToe"},
+#   {20, "LSmallToe"},
+#   {21, "LHeel"},
+#   {22, "RBigToe"},
+#   {23, "RSmallToe"},
+#   {24, "RHeel"},
+#   {25, "Background"}
+
  
 parser = argparse.ArgumentParser(description='Predict images using camera')
 parser.add_argument("--json", dest="jsonPath", type=str, default="jsonFiles/", help="Directory where save .json files (default = jsonFiles/)")
@@ -20,6 +48,7 @@ class Point():
     def __init__(self,pt):
         # Convert to numpy float or there will be error when printing lines
         pt =  np.float32(pt)
+        self.npPoint = pt[0:2]
         self.x = pt[0]
         self.y = pt[1]
         self.score = pt[2]
@@ -29,6 +58,26 @@ class Point():
         else:
             self.draw = False
    
+class Pose:
+    def __init__(self,keypoints):
+        self.rightShoulder = Point(keypoints[2])
+        self.rightElbow = Point(keypoints[3])
+        self.rightWrist = Point(keypoints[4])
+        self.leftShoulder = Point(keypoints[5])
+        self.leftElbow = Point(keypoints[6])
+        self.leftWrist = Point(keypoints[7])
+        self.center = Point(keypoints[0])
+    
+# Convert points to reference frame placed in middle hip
+def convertToRefFrame(ref, point):
+    changeValue = np.float32([1, -1])
+    center = ref.npPoint
+    newPoint = point.npPoint - center
+    newPoint = newPoint*changeValue
+    newPoint = np.flip(newPoint,0)
+    return newPoint
+    
+
     
 def setParams():
     args = parser.parse_args()
@@ -43,8 +92,16 @@ def setParams():
     return params
 
 def setReferenceFrame(data):
-    persons = len(data.poseKeypoints)
-    for i in range(persons):
+    peopleNum = len(data.poseKeypoints)
+    for i in range(peopleNum):
+        # center = Point(data.poseKeypoints[i][0])
+        pose = Pose(data.poseKeypoints[i])
+        print("Center: "+str(pose.center.npPoint))
+        print("RightShoulder: "+str(pose.rightShoulder.npPoint))
+        print("Right Shoulder fixed: "+str(convertToRefFrame(pose.center, pose.rightShoulder)))
+
+        # print("x: "+str(center.x)+" y: "+str(center.y))
+        # print("Array: "+str(center.npPoint))
     try:
         print("First dimension : ")
         print(data.poseKeypoints[1][1])
